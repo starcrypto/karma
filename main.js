e4825039cf0fc0f33258c9e4dd79ff2f000e7289
c9s <yoan@maicoin.com>
2022-04-14 13:15:34 +0800

    add issue comment handler

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ const fetch = require('node-fetch');
 
 const parseDiff = require('parse-diff');
 
+const {Octokit} = require("@octokit/rest");
+
 const {Webhooks, createNodeMiddleware} = require("@octokit/webhooks");
+
+const octokit = new Octokit({
+    auth: process.env.GITHUB_PERSONAL_TOKEN,
+});
+
 const webhooks = new Webhooks({
     secret: process.env.GITHUB_WEBHOOK_SECRET
 });
@@ -21,6 +28,31 @@ webhooks.on([
 })
 
 webhooks.on([
+    "pull_request.opened"
+], ({id, name, payload}) => {
+    const issueNumber = payload.number;
+    const userLogin = payload.pull_request.user.login;
+    const owner = payload.pull_request.base.user
+    const baseRepo = payload.pull_request.base.repo
+
+    const comment = `Hi @${userLogin},
+To receive BBG token, please left your polygon address in the following format, e.g.,
+ 
+        polygon:0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B
+      
+Once this pull request is merged, your BBG token will be sent to your wallet.
+`
+
+    const resp = octokit.rest.issues.createComment({
+        owner,
+        baseRepo,
+        issueNumber,
+        comment,
+    });
+    console.log(resp);
+})
+
+webhooks.on([
     "pull_request.opened",
     "pull_request.reopened",
     "pull_request.synchronize",
@@ -30,13 +62,15 @@ webhooks.on([
 
     const userLogin = payload.pull_request.user.login;
     const diffUrl = payload.pull_request.diff_url;
-    const pullRequestNo = payload.number;
+    const issueNumber = payload.number;
     const state = payload.pull_request.state;
 
     const response = await fetch(diffUrl);
     const diffText = await response.text();
     const files = parseDiff(diffText);
     console.log(files);
+
+
 });
 
 

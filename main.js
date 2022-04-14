029a95e7f50317cc938694107a8acb897eda6fc2
c9s <yoan@maicoin.com>
2022-04-14 16:01:09 +0800

    use web3 to sign tx to transfer

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const Web3 = require('web3');
 
+
 const fetch = require('node-fetch');
 
 const parseDiff = require('parse-diff');
@@ -9,6 +10,90 @@ const parseDiff = require('parse-diff');
 const {Octokit} = require("@octokit/rest");
 
 const {Webhooks, createNodeMiddleware} = require("@octokit/webhooks");
+
+const {ERC20PresetMinterPauser} = require("./abi/ERC20PresetMinterPauser");
+
+// const { ethers } = require("ethers");
+// const ethersPolygonHttpProvider = new ethers.providers.JsonRpcProvider(polygonRPCUrl);
+// const signer = ethersPolygonHttpProvider.getSigner();
+
+const polygonRPCUrl = "https://polygon-rpc.com";
+const polygonHttpProvider = new Web3.providers.HttpProvider(polygonRPCUrl);
+
+// const web3 = new Web3(polygonProvider Web3.givenProvider);
+const web3 = new Web3(polygonHttpProvider);
+const ownerAddress = process.env.CONTRACT_OWNER_ADDRESS;
+const contractAddress = process.env.CONTRACT_ADDRESS;
+const privateKey = process.env.PRIVATE_KEY;
+
+// for testing (vitalik address)
+const recipientAddress = "0xc1e42f862d202b4a0ed552c1145735ee088f6ccf";
+
+const tokenContract = new web3.eth.Contract(ERC20PresetMinterPauser.abi, contractAddress, {from: ownerAddress});
+
+
+const signTransaction = async (tokenContract, fromPrivateKey, toAddress, balance) => {
+    const encodedTransferABI = tokenContract.methods.transfer(toAddress, balance).encodeABI();
+
+    const estimatedGas = await tokenContract.methods.transfer(toAddress, balance).estimateGas({
+        to: contractAddress,
+        data: encodedTransferABI,
+    });
+
+    const Tx = {
+        // We are sending request to WETH contract, asking it to transfer tokens.
+        to: contractAddress,
+        gas: estimatedGas,
+        // We send zero native token, which is Matic on Polygon network.
+        value: "0x0",
+        // We set only tip for the miner, baseFeePerGas(which is burned) will be set automatically.
+        // In order to send legacy transaction(before London fork), you can use gas and gasPrice.
+        maxPriorityFeePerGas: await web3.eth.getGasPrice(),
+        data: `${encodedTransferABI}`,
+    };
+
+    const signTransactionOutput = await web3.eth.accounts.signTransaction(
+        Tx,
+        fromPrivateKey
+    );
+
+    return signTransactionOutput.rawTransaction;
+};
+
+const sendTransaction = (rawTransaction) => {
+    return web3.eth.sendSignedTransaction(
+        rawTransaction
+    ).once('transactionHash', (function (hash) {
+        console.log(`tx hash: ${hash}`)
+    }))
+        .on('confirmation', function (confNumber, receipt) {
+            console.log(`Confirmation: ${confNumber}`);
+        })
+        .on('error', async function (error) {
+            console.log('something went wrong...', error);
+        });
+}
+
+const sendMain = async () => {
+    const totalBalance = await tokenContract.methods.balanceOf(ownerAddress).call();
+    console.log("BBG", totalBalance);
+    const amount = web3.utils.toWei('1000');
+    const rawTransaction = await signTransaction(tokenContract, privateKey, recipientAddress, amount);
+    sendTransaction(rawTransaction);
+};
+sendMain();
+
+/*
+const createTransaction = await web3.eth.accounts.signTransaction(
+    {
+        from: ownerAddress,
+        to: recipientAddress,
+        value: web3.utils.toWei('100', 'ether'),
+        gas: '21000',
+    },
+    privateKey
+);
+*/
 
 const octokit = new Octokit({
     auth: process.env.GITHUB_PERSONAL_TOKEN,
